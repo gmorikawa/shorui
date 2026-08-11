eaee10b634a90af980ff7f47d93f3214b3e33a7e
gmorikawa <morikawa.gabriel@outlook.jp>
2026-08-11 19:15:00 +0900

    feat: hid timestamp metadata

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,6 +26,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'created_at',
+        'updated_at',
     ];
 
     protected $casts = [
